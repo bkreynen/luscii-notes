974e50ccf0e823c37068a3ee09a91405bbe9afa7
Bernd Kreynen <bernd@burntsoftware.nl>
2026-01-07 20:13:39 +0100

    Add tests for deleting notes

--- tst/testUtils.ts
+++ tst/testUtils.ts
@@ -7,18 +7,26 @@ import { NotesRepository } from '../src/types'
 export type TestApp = {
   app: Express
   postNote: (payload: any) => request.Test
+  deleteNote: (id: string) => request.Test
 }
 
 export function createTestApp(repo: NotesRepository = new InMemoryNotesRepository()): TestApp {
   const app = createApp(repo)
   const postNote = (payload: any) => request(app).post('/notes').send(payload)
-  return { app, postNote }
+  const deleteNote = (id: string) => request(app).delete(`/notes/${id}`)
+  return { app, postNote, deleteNote }
 }
 
 export function createErrorTestApp(): TestApp {
   // A repository that always throws an error when saving a note
   const errorRepo: NotesRepository = {
     async saveNote() {
+      return Promise.reject(new Error('Simulated repository error'))
+    },
+    async deleteNote(id: string) {
+      return Promise.reject(new Error('Simulated repository error'))
+    },
+    async getNote(id: string) {
       return Promise.reject(new Error('Simulated repository error'))
     },
   }
