ad00b80e04d3cb6afa496ca28214407c5f308e9b
Bernd Kreynen <bernd@burntsoftware.nl>
2026-01-07 20:15:04 +0100

    Update repo and service

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,8 @@ export interface NotesService {
 
 export interface NotesRepository {
   saveNote(note: Note): Promise<void>
+  deleteNote(id: string): Promise<void>
+  getNote(id: string): Promise<Note | null>
 }
 
 export interface Note {
